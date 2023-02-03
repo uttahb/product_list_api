35d783503f47ae5f6251a2b55bdb91bbcaa89644
Jishnu <jishnu.nb@neoito.com>
2023-02-03 12:52:11 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#product_list_api
---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const product_list_api = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello product_list_api`}))
+  res.end()
+  
+}
+
+export default product_list_api
